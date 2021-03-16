269d1b220e1254c56135cfe34b3535a848020670
Tiffany Yang <s10050242@gmail.com>
2021-03-16 11:41:18 +0800

    Initial commit

---
+++ Black Jack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Black Jack
+//
+//  Created by Tiff Yang on 2021/3/16.
+//  Copyright © 2021 Tiffany Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
